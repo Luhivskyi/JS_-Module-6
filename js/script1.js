8d8a173e944984c8858b8864a2add2c789f93f13
Luhivskyi <luhivskyi.d@ukr.net>
2021-01-26 13:20:37 +0300

    Метод forEach(callback) 1
    Функция calculateTotalPrice(orderedItems) принимает один параметр orderedItems - массив чисел, и рассчитывает общую сумму его элементов, которая сохраняется в переменной totalPrice и возвращается как результат работы функции.
    
    Выполни рефакторинг функции так, чтобы вместо цикла for она использовала метод forEach.

---
+++ js/script1.js
@@ -0,0 +1,12 @@
+
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  // Пиши код ниже этой строки
+
+ orderedItems.forEach(function calculateTotalPrice(orderedItems)  {
+    totalPrice += orderedItems;
+  });
+
+  // Пиши код выше этой строки
+  return totalPrice;
+}
