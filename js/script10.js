38b4713365466c85dc08f47725120662b69e9c26
Luhivskyi <luhivskyi.d@ukr.net>
2021-01-28 14:32:48 +0300

    Метод map( )  10
    Дополни код так, чтобы в переменной planetsLengths получился массив длин названий планет. Обязательно используй метод map().

---
+++ js/script10.js
@@ -0,0 +1,5 @@
+const planets = ['Земля', 'Марс', 'Венера', 'Юпитер'];
+// Пиши код ниже этой строки
+const planetsLengths = planets.map(function(planets, index, planets){
+return planets[index].length
+});
